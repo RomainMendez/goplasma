e1452adfde802e957f89c27ad068661cc6d5306f
Romain Mendez <romain.mendez@protonmail.com>
2020-04-06 15:40:01 +0200

    about to deploy

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ def default():
 @app.route('/result')
 def result():
     score = scorer(request.args)
-    print(request.args)
     print(score)
     return render_template("result.html", score=score)
 
@@ -25,3 +24,5 @@ if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
+
+
